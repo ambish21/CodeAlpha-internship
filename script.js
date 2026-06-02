dd972a01d50a4bc729822241ffec96282b094ac0
Ambish21 <ambishyaqoob2005@outlook.com>
2026-06-02 22:35:27 +0500

    done

--- script.js
+++ script.js
@@ -1,47 +1,82 @@
-console.log("JS file loaded");
-function calculateAge() {
+function calculateAge(){
 
     let dob = document.getElementById("dob").value;
 
-    if (dob === "") {
+    if(dob === ""){
         alert("Please select your date of birth");
         return;
     }
 
-    let birthDate = new Date(dob);
+    let birth = new Date(dob);
     let today = new Date();
 
-    let years = today.getFullYear() - birthDate.getFullYear();
-    let months = today.getMonth() - birthDate.getMonth();
-    let days = today.getDate() - birthDate.getDate();
+    let years = today.getFullYear() - birth.getFullYear();
 
-    if (days < 0) {
+    let months = today.getMonth() - birth.getMonth();
+    let days = today.getDate() - birth.getDate();
+
+    if(days < 0){
         months--;
         days += 30;
     }
 
-    if (months < 0) {
+    if(months < 0){
         years--;
         months += 12;
     }
 
     let message = "";
+    let color = "";
 
-    if (years <= 15) {
-        message = "👶 Childhood time — enjoy learning and fun!";
+    // 👶 1 - 18 (PINK)
+    if(years >= 1 && years <= 18){
+        message = "🌸 Keep growing, learning and enjoying your childhood journey!";
+        color = "#ffb6c1";
     }
-    else if (years <= 40) {
-        message = "🚀 Prime time — build your future!";
+
+    // 🧑 18 - 40 (YELLOW)
+    else if(years > 18 && years <= 40){
+        message = "✨ Adult life is about building dreams, stay focused and strong!";
+        color = "#fff3a0";
     }
-    else {
-        message = "👴 Wisdom stage — your experience inspires others!";
+
+    // 👴 40 - 100 (BLUE)
+    else{
+        message = "💙 Your experience is your power. You are truly inspiring!";
+        color = "#93c5fd";
     }
 
     let result = document.getElementById("result");
 
+    result.style.background = color;
+
     result.innerHTML = `
-        <h3>Your Age</h3>
-        <p><b>${years}</b> Years, <b>${months}</b> Months, <b>${days}</b> Days</p>
+        <p>Age: ${years} Years, ${months} Months, ${days} Days</p>
+        <br>
         <p>${message}</p>
     `;
 }
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 60
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle"
+    },
+    "opacity": {
+      "value": 0.5
+    },
+    "size": {
+      "value": 3
+    },
+    "move": {
+      "enable": true,
+      "speed": 2
+    }
+  }
+});
